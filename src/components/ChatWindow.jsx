18aab858698f6cd7eb6ddfd3c0b4de1fb2a4c34a
PranavShende-codes <pranavshede40@gmail.com>
2025-03-13 16:00:41 +0530

    Revert "Added Chatcontext and implemented input logic"
    This reverts commit a0a813c5cd8cfc9871b117029b3527856b3957f3.

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -1,32 +1,9 @@
-import { useContext } from "react";
-import { ChatContext } from "../Contexts/ChatContext.jsx";
 import ChatInput from "./ChatInput";
-
 export default function ChatWindow() {
-  const contextValue = useContext(ChatContext); // ✅ Check if context is available
-  console.log("ChatContext Value:", contextValue); // ✅ Debugging
-
-  if (!contextValue) {
-    return <div>Error: ChatContext is not available!</div>; // Show error if context is missing
-  }
-
-  const { messages } = contextValue; // ✅ Destructuring after checking
-
-  return (
-    <div className="flex-1 overflow-y-auto bg-white h-full p-6 relative">
-      {messages.map((msg, index) => (
-        <div
-          key={index}
-          className={`p-3 rounded-lg ${
-            msg.sender === "user"
-              ? " self-end"
-              : "bg-gray-200 text-black"
-          }`}
-        >
-          {msg.text}
-        </div>
-      ))}
-      <ChatInput />
-    </div>
-  );
+ return (
+  <div className="flex-1 overflow-y-auto bg-white h-full p-6 relative">
+   <p>hello Chat</p>
+   <ChatInput />
+  </div>
+ );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import React from 'react'
 import './index.css'
 import App from './App.jsx'
-import { ChatProvider } from './Contexts/ChatContext'
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ChatProvider>
-      <App />
-    </ChatProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
